2ae776ee6305dbaf76b9d7ae8207a08c82d1a18e
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2021-06-12 21:41:39 -0300

    added the drag and drop section

--- js/w08.js
+++ js/w08.js
@@ -1,3 +1,7 @@
+/**********************************************************
+ Canvas functions
+**********************************************************/
+
 // Get the canvas and context
 let canvas = document.querySelector('#c1');
 let context = canvas.getContext("2d");
@@ -41,6 +45,52 @@ drawPattern();
 
 const clearCanvas = () => {
   var canvas = document.getElementById("c1");
-   canvas.width = canvas.width;
+  canvas.width = canvas.width;
 }
 
+/**********************************************************
+ Drag and Drop
+**********************************************************/
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.drag-cont');
+
+containers.forEach(c => {
+  c.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const isDragged = document.querySelector('.dragging');
+    c.appendChild(isDragged);
+  });
+})
+
+draggables.forEach(d => {
+  d.addEventListener('dragstart', (e) => {
+    e.target.classList.add('dragging');
+  });
+  d.addEventListener('dragend', (e) => {
+    e.target.classList.remove('dragging');
+    if (checkGroups()) {
+      containers.forEach(c => {
+        c.classList = 'correct';
+      });
+      draggables.forEach(d=>{
+        d.setAttribute('draggable', false);
+        d.classList.remove('draggable');
+      })
+    }
+  });
+})
+
+const checkGroups = () => {
+  let supers = ['Venom', 'Constantine', 'Iron Man', 'Thor', 'Wolverine'];
+  let marvel = document.querySelector('.marvel').children;
+  let confirmed = 0;
+  for (let i = 0; i < marvel.length; i++) {
+    if (!supers.includes(`${marvel[i].innerHTML}`)) {
+      confirmed = 0;
+      break;
+    };
+    console.log(` ${marvel[i].innerHTML} is a Marvel character: ${supers.includes(marvel[i].innerHTML)}`);
+    confirmed++;
+  }
+  return confirmed === 4;
+};
